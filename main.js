4401d30b2fc7c11e953c2c240786643182123ef9
odify <odify495@gmail.com>
2020-03-23 18:53:33 +0100

    Under construction...

---
+++ main.js
@@ -0,0 +1,14 @@
+// STILL UNDER CONSTRUCTION
+
+let x=document.getElementsByName("text").innerHTML;
+let y=document.getElementById("key").value;
+
+let arr=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+x.addEventListener(onlick, myEncode()) {
+
+    console.log(x)
+
+}
+
